02949d09f849d3d0dfeb6b4f30c130c5c5430e05
Grigor Barseghyan <grigorb@parasoft.com>
2017-07-11 14:27:47 -0700

    @task SOA-8511 - Create skeleton project
    Reviewers: benken
    
    Reviewed By: benken
    
    Differential Revision: http://phabricator.parasoft.com/D1701

---
+++ src/main/java/com/parasoft/findings/bamboo/SOAtest9xReportParserTask.java
@@ -0,0 +1,15 @@
+package com.parasoft.findings.bamboo;
+
+import com.atlassian.bamboo.task.TaskContext;
+import com.atlassian.bamboo.task.TaskException;
+import com.atlassian.bamboo.task.TaskResult;
+import com.atlassian.bamboo.task.TaskType;
+
+public class SOAtest9xReportParserTask implements TaskType {
+
+	public TaskResult execute(TaskContext arg0) throws TaskException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
