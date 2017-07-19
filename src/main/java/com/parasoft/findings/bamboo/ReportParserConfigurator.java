1bda2839d92a19b918fc2fd4280f8b8c01492708
Grigor Barseghyan <grigorb@parasoft.com>
2017-07-19 09:28:43 -0700

    @task SOA-8579 - Validate report location
    Summary: Use Atlassian scanner version2, to inject I18nResolver and TaskConfiguratorHelper components.
    
    Reviewers: benken
    
    Reviewed By: benken
    
    Differential Revision: http://phabricator.parasoft.com/D1727

--- src/main/java/com/parasoft/findings/bamboo/ReportParserConfigurator.java
+++ src/main/java/com/parasoft/findings/bamboo/ReportParserConfigurator.java
@@ -3,22 +3,33 @@ package com.parasoft.findings.bamboo;
 import com.atlassian.bamboo.collections.*;
 import com.atlassian.bamboo.task.*;
 import com.atlassian.bamboo.utils.error.*;
+import com.atlassian.plugin.spring.scanner.annotation.imports.*;
 import com.atlassian.sal.api.message.*;
 
 import java.util.*;
+
+import javax.inject.*;
 
 import org.jetbrains.annotations.*;
 
 public class ReportParserConfigurator extends AbstractTaskConfigurator implements TaskTestResultsSupport {
     private static final String DEFAULT_REPORT_LOCATION = "**/report.xml";
+    private static final List<String> FIELD_KEYS =
+            Arrays.asList(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN, TaskConfigConstants.CFG_TEST_OUTDATED_RESULTS_FILE);
     private I18nResolver i18nResolver;
+
+    @Inject
+    public ReportParserConfigurator(@ComponentImport I18nResolver i18nResolver,
+            @ComponentImport TaskConfiguratorHelper taskConfiguratorHelper) {
+        this.i18nResolver = i18nResolver;
+        this.taskConfiguratorHelper = taskConfiguratorHelper;
+    }
 
     @Override
     public Map<String, String> generateTaskConfigMap(@NotNull final ActionParametersMap params,
             @Nullable final TaskDefinition previousTaskDefinition) {
         final Map<String, String> config = super.generateTaskConfigMap(params, previousTaskDefinition);
-        config.put(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN, params.getString(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN));
-        config.put(TaskConfigConstants.CFG_TEST_OUTDATED_RESULTS_FILE, params.getString(TaskConfigConstants.CFG_TEST_OUTDATED_RESULTS_FILE));
+        taskConfiguratorHelper.populateTaskConfigMapWithActionParameters(config, params, FIELD_KEYS);
         return config;
     }
 
@@ -33,17 +44,13 @@ public class ReportParserConfigurator extends AbstractTaskConfigurator implement
     public void populateContextForEdit(@NotNull final Map<String, Object> context,
             @NotNull final TaskDefinition taskDefinition) {
         super.populateContextForEdit(context, taskDefinition);
-        context.put(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN,
-                taskDefinition.getConfiguration().get(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN));
-        context.put(TaskConfigConstants.CFG_TEST_OUTDATED_RESULTS_FILE,
-                taskDefinition.getConfiguration().get(TaskConfigConstants.CFG_TEST_OUTDATED_RESULTS_FILE));
+        taskConfiguratorHelper.populateContextWithConfiguration(context, taskDefinition, FIELD_KEYS);
     }
 
-    // NYI. See task SOA-8579, remove i18nResolver != null
     @Override
     public void validate(@NotNull ActionParametersMap params, @NotNull ErrorCollection errorCollection) {
         String reportLocation = params.getString(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN);
-        if (i18nResolver != null && (reportLocation == null || reportLocation.trim().length() == 0)) {
+        if (reportLocation == null || reportLocation.trim().length() == 0) {
             errorCollection.addError(TaskConfigConstants.CFG_TEST_RESULTS_FILE_PATTERN,
                     i18nResolver.getText("report.location.pattern.error"));
         }
@@ -51,10 +58,5 @@ public class ReportParserConfigurator extends AbstractTaskConfigurator implement
 
     public boolean taskProducesTestResults(TaskDefinition taskDefinition) {
         return true;
-    }
-
-    // NYI. See task SOA-8579
-    public void setI18nResolver(I18nResolver i18nResolver) {
-        this.i18nResolver = i18nResolver;
     }
 }
